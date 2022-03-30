9fc02196096f89ceaee3beeffdd86de80d993c5b
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2022-03-30 12:40:24 +1100

    terrible cleanup option

--- app/pykrotik.py
+++ app/pykrotik.py
@@ -2,7 +2,8 @@
 """Backup a list of mikrotiks"""
 import os
 import sys
-import datetime
+from os import path
+from datetime import datetime, timedelta, date
 import argparse
 import logging
 import paramiko
@@ -35,6 +36,29 @@ def _do_paramiko_exec(pclient: paramiko.SSHClient, cmd: str) -> tuple():
         remote_cmd
     )
 
+
+def _cleanup(olderthan: int):
+    """Find and remove backup files older than X days old
+
+    Args:
+        olderthan (int): Files older than this many days will be removed
+    """
+    days_ago = datetime.now() - timedelta(days=olderthan)
+
+    with os.scandir(args.output) as filescan:
+        for entry in filescan:
+            if (
+                entry.is_file()
+                and entry.name.endswith(".rcs")
+                or entry.name.endswith(".backup")
+            ):
+                filetime = datetime.fromtimestamp(path.getctime(entry))
+                if filetime < days_ago:
+                    if args.verbose:
+                        print(f"Removing {entry.name} - dated {filetime}")
+                    os.remove(entry)
+
+
 parser = argparse.ArgumentParser(description="Shoddy MikroTik backups")
 parser.add_argument(
     "-v",
@@ -51,6 +75,14 @@ parser.add_argument(
     type=str,
     default="devices.yaml",
     help="Path to devices.yaml",
+)
+parser.add_argument(
+    "-c",
+    "--clean",
+    required=False,
+    default=False,
+    type=int,
+    help="cleanup after <int> days",
 )
 parser.add_argument("-o", "--output", required=True, type=str, help="Output Directory")
 args = parser.parse_args()
@@ -69,7 +101,11 @@ if not os.path.isdir(args.output):
     print(f"Output path - {args.output} - does not exist. Creating it")
     os.makedirs(args.output)
 
-today = (datetime.date.today()).strftime("%Y%m%d")
+if args.clean:
+    _cleanup(args.clean)
+
+today = date.today()  # .strftime("%Y%m%d")
+
 # loop over the config file
 for device in config:
     remoteDevice = config[device]
@@ -86,18 +122,17 @@ for device in config:
         look_for_keys=False,
     )
 
-    for cmds in [f"/export compact file=export-{today}.rsc",
-                f"/system backup save name=backup-{today}"]:
+    for cmds in [
+        f"/export compact file=export-{today}.rsc",
+        f"/system backup save name=backup-{today}",
+    ]:
         success, res = _do_paramiko_exec(client, cmds)
         if not success and res[2] and args.verbose:
-            print(f'Command - {cmds} - errors.\n{res[2]}')
+            print(f"Command - {cmds} - errors.\n{res[2]}")
 
-    for files in [
-        f"export-{today}.rsc",
-        f"backup-{today}.backup"
-    ]:
+    for files in [f"export-{today}.rsc", f"backup-{today}.backup"]:
         sftp_client = client.open_sftp()
-        sftp_client.get(files, f'{args.output}/{device}-{files}')
+        sftp_client.get(files, f"{args.output}/{device}-{files}")
         sftp_client.close()
 
     client.close()
